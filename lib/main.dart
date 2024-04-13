e74b874d90df2b031d8f06255905ca67350724da
ivansaul <saul_2503@hotmail.com>
2024-04-12 19:52:32 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Open Stack',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Open Stack'),
+        ),
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
